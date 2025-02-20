48f2948097142c13c9232b10428a79acea53d882
Mark Franco <mark.franco@microsoft.com>
2025-02-20 17:30:47 -0500

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,25 @@
+# Use an appropriate base image
 FROM ubuntu:latest
 
-RUN apt-get update && apt-get upgrade -y
+# Install dependencies
+RUN apt-get update && apt-get install -y \
+    curl \
+    wget \
+    fuse \
+    blobfuse=1.4.4
 
+# Create necessary directories
+RUN mkdir -p /mnt/blobfusetmp /mnt/blobfuseblockcache /mnt/blobfusefilecache
+
+# Set environment variables for BlobFuse
+ENV AZURE_STORAGE_ACCOUNT=<your-storage-account-name>
+ENV AZURE_STORAGE_ACCESS_KEY=<your-storage-account-key>
+
+# Copy your application code
+COPY . /app
+
+# Set the working directory
+WORKDIR /app
+
+# Run your application
 CMD ["bash", "-c", "date"]
--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,9 @@ az aks update --resource-group "myAKSResourceGroup0472af" --name "myAKSCluster04
 # recycle nodes to accept system Managed identity security scheme
 az aks nodepool upgrade --resource-group "myAKSResourceGroup0472af" --cluster-name "myAKSCluster0472af" --name nodepool1 --node-image-only
 
+# add the Blob CSI Driver
+az aks update --enable-blob-driver --name myAKSCluster0472af --resource-group myAKSResourceGroup0472af
+
 # Persistant Volume Storage Creation
 # Get the node resource group name for reference later
 export NODEPOOL_RESOURCE_GROUP_NAME=$(az aks show --resource-group myAKSResourceGroup0472af --name myAKSCluster0472af --query nodeResourceGroup -o tsv)
@@ -30,19 +33,11 @@ export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-stri
 az storage share create -n datavolve-container --connection-string $AZURE_STORAGE_CONNECTION_STRING
 
 # Get the principal ID for a system-assigned managed identity.
-export CLUST_OBJECT_ID=$(az aks show --name myAKSCluster0472af --resource-group myAKSResourceGroup0472af --query identity.principalId --output tsv)
-export CLUSTER_APP_ID=$(az ad sp show --id $CLUST_OBJECT_ID --query appId --output tsv)
-
-az role assignment create --assignee $CLUST_OBJECT_ID --role "Network Contributor" --scope "/subscriptions/28d10200-70b0-476c-b004-c6ae29265897/resourceGroups/MC_myAKSResourceGroup0472af_myAKSCluster0472af_canadacentral/providers/Microsoft.Storage/storageAccounts/sadatavolveaccount"
-
-# [IMPORTANT] Assign the client ID to inside the azure-storage-csi.yaml and azurefiles-pv.yaml files
-
-# [IMPORTANT] USE THE PORTAL TO DO THIS FOR NOW: In the storage account Add the system assigned identity as the Storage file Data Contrubutor
-# add this one liner to the script to automate this
-# ccae798c-fb8d-42a6-87df-e88ba4c11370az ad sp show --id <ServicePrincipalObjectId>
-az role assignment create --assignee-object-id $CLUST_OBJECT_ID --assignee-principal-type ServicePrincipal --role "Storage Blob Data Contributor" --scope "/subscriptions/28d10200-70b0-476c-b004-c6ae29265897/resourceGroups/MC_myAKSResourceGroup0472af_myAKSCluster0472af_canadacentral/providers/Microsoft.Storage/storageAccounts/sadatavolveaccount" --subscription "28d10200-70b0-476c-b004-c6ae29265897"
-az role assignment create --assignee-object-id $CLUST_OBJECT_ID --assignee-principal-type ServicePrincipal --role "Storage File Data SMB Share Contributor" --scope "/subscriptions/28d10200-70b0-476c-b004-c6ae29265897/resourceGroups/MC_myAKSResourceGroup0472af_myAKSCluster0472af_canadacentral/providers/Microsoft.Storage/storageAccounts/sadatavolveaccount" --subscription "28d10200-70b0-476c-b004-c6ae29265897"
-
+export KUBELET_USER_ASSIGNED_IDENTITY_CLIEND_ID=$(az aks show --name myAKSCluster0472af --resource-group myAKSResourceGroup0472af --query identityProfile.kubeletidentity.clientId --output tsv)
+# 261ac719-91e0-438d-aebc-7e5687c074fb
+# az role assignment create --assignee $KUBELET_USER_ASSIGNED_IDENTITY_CLIEND_ID --role "Network Contributor" --scope "/subscriptions/28d10200-70b0-476c-b004-c6ae29265897/resourceGroups/MC_myAKSResourceGroup0472af_myAKSCluster0472af_canadacentral/providers/Microsoft.Storage/storageAccounts/sadatavolveaccount"
+az role assignment create --assignee $KUBELET_USER_ASSIGNED_IDENTITY_CLIEND_ID --role "Storage Blob Data Contributor" --scope "/subscriptions/28d10200-70b0-476c-b004-c6ae29265897/resourceGroups/MC_myAKSResourceGroup0472af_myAKSCluster0472af_canadacentral/providers/Microsoft.Storage/storageAccounts/sadatavolveaccount" --subscription "28d10200-70b0-476c-b004-c6ae29265897"
+az role assignment create --assignee $KUBELET_USER_ASSIGNED_IDENTITY_CLIEND_ID --role "Storage File Data SMB Share Contributor" --scope "/subscriptions/28d10200-70b0-476c-b004-c6ae29265897/resourceGroups/MC_myAKSResourceGroup0472af_myAKSCluster0472af_canadacentral/providers/Microsoft.Storage/storageAccounts/sadatavolveaccount" --subscription "28d10200-70b0-476c-b004-c6ae29265897"
 
 # Create the persistant Volume
 kubectl create -f azure-storage-pv.yaml
@@ -76,3 +71,6 @@ az storage share create -n datavolve-container --connection-string $AZURE_STORAG
 #TODO: Move away from the System Managed Identity and try at the POD level here: https://learn.microsoft.com/en-us/azure/aks/use-azure-ad-pod-identity
 
 #  If pod level droesn;t work with storageclass, then go back to user manmaged idetity
+
+mount -t cifs //sadatavolveaccount.file.core.windows.net/datavolve-container /mnt/datavolve-container -o dir_mode=0777,file_mode=0777,uid=0,gid=0,mfsymlinks,cache=strict,nosharesock,nobrl,actimeo=30,vers=3.0
+mount -t cifs -o dir_mode=0777,file_mode=0777,uid=0,gid=0,mfsymlinks,cache=strict,nosharesock,nobrl,actimeo=30,vers=3.0 //sadatavolveaccount.file.core.windows.net/datavolve-container /mnt/datavolve-container
